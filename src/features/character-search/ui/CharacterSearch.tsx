c21c2cbde33a0954629dd94a8acc74723bf3e3af
naritai <alexandr.voronetskiy@gmail.com>
2023-06-07 15:11:13 +0300

    debounce search character input

--- src/features/character-search/ui/CharacterSearch.tsx
+++ src/features/character-search/ui/CharacterSearch.tsx
@@ -1,12 +1,12 @@
 import { classNames } from 'shared/lib/classNames/';
 import cls from './CharacterSeacrh.module.scss';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from 'app/providers/store-provider';
 import { fetchCharacters } from 'entities/character/api';
 import { Input } from '@mui/material';
-import { charactersActions, selectCharactersState } from 'entities/character';
-import { useSelector } from 'react-redux';
+import { charactersActions } from 'entities/character';
+import { useDebounce } from 'usehooks-ts';
 
 interface CharacterSeacrhProps { 
   className?: string; 
@@ -14,16 +14,21 @@ interface CharacterSeacrhProps {
 
 export function CharacterSearch({ className }: CharacterSeacrhProps): JSX.Element {
   const dispatch = useDispatch<AppDispatch>();
-  const { search } = useSelector(selectCharactersState);
+  const [searchValue, setSearchValue] = useState<string>('');
+  const debouncedValue = useDebounce<string>(searchValue, 500);
 
-  const handleSearchCharacter = (event: ChangeEvent<HTMLInputElement>) => {
-    dispatch(charactersActions.searchUpdated(event.target.value));
+  useEffect(() => {
+    dispatch(charactersActions.searchUpdated(debouncedValue));
     dispatch(fetchCharacters());
+  }, [debouncedValue, dispatch])
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
   }
 
   return (
     <div className={classNames(cls.characterseacrh, {}, [className])}>
-      <Input value={search} placeholder="search character by name" onChange={handleSearchCharacter} />
+      <Input value={searchValue} placeholder="search character by name" onChange={handleSearch} />
     </div>
   )
 }
