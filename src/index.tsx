73de01a4ed99300844f2c4caebfb97c09e5aa306
naritai <alexandr.voronetskiy@gmail.com>
2023-06-08 01:07:00 +0300

    disable react dev tools in production

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,13 @@ import { themeOptions } from 'shared/config/themeConfig/themeConfig';
 import { StoreProvider } from 'app/providers/store-provider';
 import { ErrorBoundary } from 'app/providers/error-boundary';
 import { ErrorPage } from 'pages/error-page';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import { NODE_ENVS } from 'shared/types/types';
 import './app/styles/index.scss';
+
+if (process.env.NODE_ENV === NODE_ENVS.PROD) {
+  disableReactDevTools();
+}
 
 const theme = createTheme(themeOptions);
 
