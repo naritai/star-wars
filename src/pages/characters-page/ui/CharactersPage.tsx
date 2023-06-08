fb34623605c4230a46e7da86adc4795234bb25cb
naritai <alexandr.voronetskiy@gmail.com>
2023-06-08 09:21:49 +0300

    turn off eslint rules that might conflict with prettier

--- src/pages/characters-page/ui/CharactersPage.tsx
+++ src/pages/characters-page/ui/CharactersPage.tsx
@@ -1,14 +1,14 @@
-import { classNames } from "shared/lib/classNames";
-import { selectCharactersState } from "entities/character";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { FetchStatus } from "shared/api/types";
-import { fetchCharacters } from "entities/character/api";
-import { AppDispatch } from "app/providers/store-provider";
-import { CharacterList } from "widgets/character-list";
-import { CharacterSearch } from "features/character-search";
-import { CharactersPagination } from "features/characters-pagination";
-import cls from "./CharactersPage.module.scss";
+import { classNames } from 'shared/lib/classNames';
+import { selectCharactersState } from 'entities/character';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { FetchStatus } from 'shared/api/types';
+import { fetchCharacters } from 'entities/character/api';
+import { AppDispatch } from 'app/providers/store-provider';
+import { CharacterList } from 'widgets/character-list';
+import { CharacterSearch } from 'features/character-search';
+import { CharactersPagination } from 'features/characters-pagination';
+import cls from './CharactersPage.module.scss';
 
 interface CharactersPageProps {
   className?: string;
